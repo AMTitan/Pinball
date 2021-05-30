97b65e2c18035c14997468b263305c5ccdde6fa7
Arthur Melton <amtitan@icloud.com>
2021-05-30 11:58:02 -0400

    can hit ball

--- Assets/flaper_one.cs
+++ Assets/flaper_one.cs
@@ -8,6 +8,7 @@ public class flaper_one : MonoBehaviour
     public float speed;
     private float max = -0.8001032f;
     private float min = -0.4619398f;
+    public float ballSpeed = 0;
 
     // Update is called once per frame
     void Update()
@@ -25,6 +26,27 @@ public class flaper_one : MonoBehaviour
             {
                 transform.Rotate(Vector3.up * speed * Time.deltaTime);
             }
+        }
+    }
+    
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && transform.rotation.y > max && transform.rotation.y < min && other.gameObject.name == "ball")
+        {
+            Vector3 vel = other.rigidbody.velocity;
+            vel.z = ballSpeed * -1;
+            other.rigidbody.velocity = vel;
+        }
+    }
+    
+    private void OnCollisionStay(Collision other)
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && transform.rotation.y < max && transform.rotation.y > min && other.gameObject.name == "ball")
+        {
+            Vector3 vel = other.rigidbody.velocity;
+            vel.z = ballSpeed * -1;
+            other.rigidbody.velocity = vel;
         }
     }
 }
--- Assets/flaper_two.cs
+++ Assets/flaper_two.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,11 @@ public class flaper_two : MonoBehaviour
     public float speed;
     private float max = 0.8001032f;
     private float min = 0.4619398f;
+    public float ballSpeed = 0;
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.rotation.y);
         if (Input.GetKey(KeyCode.RightArrow))
         {
             if (transform.rotation.y < max)
@@ -25,6 +26,26 @@ public class flaper_two : MonoBehaviour
             {
                 transform.Rotate(Vector3.down * speed * Time.deltaTime);
             }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && transform.rotation.y < max && transform.rotation.y > min && other.gameObject.name == "ball")
+        {
+            Vector3 vel = other.rigidbody.velocity;
+            vel.z = ballSpeed * -1;
+            other.rigidbody.velocity = vel;
+        }
+    }
+    
+    private void OnCollisionStay(Collision other)
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && transform.rotation.y < max && transform.rotation.y > min && other.gameObject.name == "ball")
+        {
+            Vector3 vel = other.rigidbody.velocity;
+            vel.z = ballSpeed * -1;
+            other.rigidbody.velocity = vel;
         }
     }
 }
