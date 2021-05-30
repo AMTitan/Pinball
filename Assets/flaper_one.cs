cc97f25907692cec63037a317d7ad7b864c86c7e
Arthur Melton <amtitan@icloud.com>
2021-05-29 22:40:09 -0400

    pinball padles

---
+++ Assets/flaper_one.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flaper_one : MonoBehaviour
+{
+
+    public float speed;
+    private float max = -0.8001032f;
+    private float min = -0.4619398f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.rotation.y > max)
+            {
+                transform.Rotate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (transform.rotation.y < min)
+            {
+                transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/flaper_two.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flaper_two : MonoBehaviour
+{
+    public float speed;
+    private float max = 0.8001032f;
+    private float min = 0.4619398f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.rotation.y);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.rotation.y < max)
+            {
+                transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (transform.rotation.y > min)
+            {
+                transform.Rotate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
+    }
+}
