35076564f0f8e45e4cb5151669add65189b1b13e
Arthur Melton <amtitan@icloud.com>
2021-05-29 21:22:04 -0400

    shoot ball

---
+++ Assets/shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+
+    private bool canRun = true;
+    private float times;
+    public float speed = 5;
+    public float time = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && canRun == true)
+        {
+            canRun = false;
+            times = Time.time + time;
+        }
+
+        if (!canRun && times > Time.time)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime * -1;
+        }
+    }
+}
---
+++ Assets/shoot2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot2 : MonoBehaviour
+{
+    private bool canRun = true;
+    public Rigidbody rb;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && canRun == true)
+        {
+            canRun = false;
+            Vector3 vel = rb.velocity;
+            vel.z = speed * -1;
+            rb.velocity = vel;
+        }
+    }
+}
